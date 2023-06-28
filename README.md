2e0d8c94ed6c62b9be9fe0856c44567500fff12e
mrJayn <100537512+mrJayn@users.noreply.github.com>
2023-06-28 16:57:38 -0400

    Pipline Added + folder structure changes

--- README.md
+++ README.md
@@ -1,25 +1,9 @@
-# ┌──────────┐<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; JYN Theme<br>└──────────┘
+# _JYN Theme_
+
+![screenshot](screenshot.png)
 
 <br>
 
-<br>
+JYN is a VSCode theme powered by `Style Dictionary` and created by Michael Jayne with project design and structure inspiration from [Nu Disco Theme](https://marketplace.visualstudio.com/items?itemName=dbanksdesign.nu-disco) by **Danny Banks**.
 
-JYN is a VSCode theme powered by Style Dictionary and created by Michael Jayne with project design and structure inspiration from _Nu Disco Theme_ by **Danny Banks**.
-
-**Check out his other stuff here!**
-
--   [Danny's GitHub ](https://github.com/dbanksdesign)
--   [Danny's VS-Code Theme Tutorial ](https://dbanks.design/blog/vs-code-theme-with-style-dictionary/)
-
-<br>
-<br>
-
-## Installation
-
-1. Install [Visual Studio Code](https://code.visualstudio.com/)
-1. Launch Visual Studio Code
-1. Choose **Extensions** from menu
-1. Search for `JYN Theme`
-1. Click **Install** to install the extension
-1. Click **Reload** to reload VS Code
-1. From the menu bar click: Code > Preferences > Color Theme > **JYN Theme**
+Check out Danny's other work >> [dbanksdesign ](https://github.com/dbanksdesign).
--- build.js
+++ build.js
@@ -2,41 +2,29 @@ const StyleDictionary = require('style-dictionary');
 const fs = require('fs');
 const path = require('path');
 
-// Get Variants
-const themesDirPath = path.join(process.cwd(), '/themes');
-const THEME_VARIANTS = fs.readdirSync(themesDirPath).filter((name) => {
-	return !['application', 'syntax', 'config.json5'].includes(name);
-});
-
 // Stringify the token object paths
 StyleDictionary.registerTransform({
 	name: 'vsCodeName',
 	type: 'name',
-	transformer: (token) => {
-		if (token.path[0] === 'syntax') {
-			if (token.name === '*') {
-				return token.path.slice(1, -1).join('.');
-			} else {
-				return token.path.slice(1).join('.');
-			}
-		} else {
-			return token.path.join('.');
-		}
+	transformer: ({ path, name }) => {
+		if (path[0] !== 'syntax') return path.join('.');
+		if (path[0] === 'syntax' && name === '*') return path.slice(1, -1).join('.');
+		return path.slice(1).join('.');
 	},
 });
 
 // Register file formatter
 StyleDictionary.registerFormat({
 	name: 'vsCodeTheme',
-	formatter: (dictionary, config) => {
-		// Base
+	formatter: (dictionary, { variant }) => {
+		const main = variant === 'jyn';
+
 		const theme = {
-			name: `JYN ${config.themeType}`,
-			type: config.themeType,
+			name: `JYN ${main ? '' : ` - ${variant}`}`,
+			type: main ? 'dark' : variant,
 			colors: {},
 		};
 
-		// Colors
 		dictionary.allProperties
 			.filter((token) => {
 				return !['color', 'theme', 'tw', 'syntax'].includes(token.path[0]);
@@ -45,11 +33,8 @@ StyleDictionary.registerFormat({
 				theme.colors[token.name] = token.value;
 			});
 
-		// Token Colors
 		theme.tokenColors = dictionary.allProperties
-			.filter((token) => {
-				return token.path[0] === 'syntax';
-			})
+			.filter(({ path }) => path[0] === 'syntax')
 			.map((token) => ({
 				scope: token.name,
 				settings: {
@@ -62,22 +47,26 @@ StyleDictionary.registerFormat({
 	},
 });
 
-//  Build each of the theme variants
-THEME_VARIANTS.forEach((themeType) => {
-	StyleDictionary.extend({
-		source: [`themes/config.json5`, `themes/${themeType}/*.json5`, `tokens/application/*.json5`, `tokens/syntax/*.json5`],
-		platforms: {
-			vscode: {
-				buildPath: `build/`,
-				themeType: themeType,
-				transforms: [`vsCodeName`],
-				files: [
-					{
-						destination: `jyn-${themeType}.color-theme.json`,
-						format: `vsCodeTheme`,
-					},
-				],
+// Get Variants & Build each one
+const themesDirPath = path.join(process.cwd(), '/themes');
+
+fs.readdirSync(themesDirPath)
+	.filter((dirent) => !['application', 'syntax', 'config.json5'].includes(dirent))
+	.forEach((theme) => {
+		StyleDictionary.extend({
+			source: [`themes/config.json5`, `themes/${theme}/*.json5`, `tokens/application/*.json5`, `tokens/syntax/*.json5`],
+			platforms: {
+				vscode: {
+					buildPath: `build/`,
+					variant: theme,
+					transforms: [`vsCodeName`],
+					files: [
+						{
+							destination: `${theme}.color-theme.json`,
+							format: `vsCodeTheme`,
+						},
+					],
+				},
 			},
-		},
-	}).buildAllPlatforms();
-});
+		}).buildAllPlatforms();
+	});
